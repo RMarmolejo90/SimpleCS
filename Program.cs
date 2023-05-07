5c0ef09b44a4bc20348d9c89c38066606dd06ecb
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-05-07 15:44:24 -0400

    added some functionality

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SimpleCS
+{
+    public class Main
+    {
+        static bool IsClockedIn { get; set; } = false;
+
+        public setClockStatus()
+        {
+            IsClockedIn == true ? IsClockedIn = false : IsClockedIn = true;
+            Console.WriteLine(IsClockedIn);
+        }
+
+
+
+        public class Shift
+        {
+            public string Date { get; set; };
+
+            public bool Overtime { get; set; };
+
+            public ClockIn
+            {
+                public string StartTime = DateTime.Now.ToString();
+                IsClockedIn = true;
+            }
+            
+            public ClockOut()
+            {
+                IsClockedIn = false
+            }
+    }
+}
