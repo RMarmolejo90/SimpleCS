c8122ad9fa39d11219ca75bc48d1d371edbf8701
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-05-08 01:35:14 -0400

    hello world method

--- Program.cs
+++ Program.cs
@@ -8,26 +8,15 @@ namespace SimpleCS
     {
         static void Main(string[] args)
         {
-            //    int x;
-            //    int y;
-
-            //    x = 7;
-            //    y = x + 3;
-
-            //    Console.WriteLine(y);
-            //    Console.ReadLine();
-
-            Console.WriteLine("What is your name?");
-            Console.Write("Type your first name: ");
-            string myFirstName;
-            myFirstName = Console.ReadLine();
-
-            string myLastName;
-            Console.Write("Type your last name: ");
-            myLastName = Console.ReadLine();
-
-            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            HelloWorld();
+            Console.ReadLine();
         }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello world");
+        }
+
     }
 
 
